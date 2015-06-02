f6ac5fe71632f8ec055b3e06225d5f53689b8ae7
Osoian Marcel <osoian.marcel.d@gmail.com>
2015-06-02 22:06:21 +0300

    New todo (app.module.js / run)

--- app/app.module.js
+++ app/app.module.js
@@ -1,13 +1,18 @@
 'use strict';
 var app = (function () {
 
-    /* App Module */
-    return angular.module('app', [
-        'ngMaterial',
-        'ui.router',
-        'ngResource',
+	/* App Module */
+	return angular.module('app', [
+		'ngMaterial',
+		'ui.router',
+		'ngResource',
 
-        'app-calls'
-    ]);
+		'app-calls'
+	]).run(['$location',
+		function ($location) {
+			// TODO: Sunt de parerea ca ar fi bine aici de verificat autentificarea
+			$location.path('/calls');
+		}
+	]);
 
 }());
